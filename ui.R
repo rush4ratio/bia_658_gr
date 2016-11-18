09b314a97bb9da0d5a21804cf6e9d0863d23fbb3
Rush Kirubi <rush4ratio@gmail.com>
2016-11-17 21:37:17 -0500

    place degree centrality table on left plane

--- ui.R
+++ ui.R
@@ -2,8 +2,9 @@ shinyUI(
   fluidPage(
     sidebarLayout(
       sidebarPanel(
+      fluidRow(
         selectInput("selectNetwork", label = h3("City"), 
-                    choices = list("Chapel Hill" = "Chapel Hill", "Hoboken" = "Hoboken", "La Crosse" = "La_Crosse",
+                    choices = list("Chapel Hill" = "Chapel Hill", "Hoboken" = "Hoboken", "La Crosse" = "La Crosse",
                                    "Lakewood" = "Lakewood", "Lancaster" = "Lancaster", "Manhattan KS"="Manhattan, KS",
                                    "New Brunswick" = "New Brunswick", "Royal Oak" = "Royal Oak", "San Marcos" = "San Marcos",
                                    "Sarasota" = "Sarasota"), 
@@ -15,12 +16,15 @@ shinyUI(
         hr(),
         checkboxInput("showLabels", label = p("Show Labels?"), value = TRUE),
         hr()
-        
+      ),
+      fluidRow(
+        tableOutput('degreeTable')
+      )
       ),
       mainPanel(tabsetPanel(
         tabPanel("Network",
-                 plotOutput("network",width = "750px", height = "600px")),
-        tabPanel("Centrality", tableOutput('degreeTable'))
+                 plotOutput("network",width = "750px", height = "600px"))
+        # tabPanel("Centrality", )
       ))
     ))
 )
